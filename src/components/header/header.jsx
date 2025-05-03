b033a9d17e636cf28609c5fddbe959bce5583a1b
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-03 15:23:18 +0200

    Add Header & Footer to Order page Header component modified

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,38 @@
 import './header.css';
 
-export const Header = () => {
-  return (
-    <header>
-      <div className="header__content container">
-        <div className="site-logo"></div>
-        <div className="navigation">
-          <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
-          </nav>
+export const Header = ({ showMenu }) => {
+  if (showMenu === true) {
+    return (
+      <header>
+        <div className="header__content container">
+          <div className="site-logo"></div>
+          <div className="navigation">
+            <button className="nav-btn"></button>
+            <nav className="rollout-nav nav-closed">
+              <a href="#home">domů</a>
+              <a href="#menu">menu</a>
+              <a href="#gallery">galerie</a>
+              <a href="#contact">kontakt</a>
+            </nav>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return (
+      <header>
+        <div className="header__content container">
+          <div className="site-logo"></div>
+          <div className="navigation">
+            <button className="nav-btn"></button>
+            <nav className="rollout-nav nav-closed">
+              <a href="/">Hlavní stránka</a>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+  }
 };
+
+// Do komponenty Header tedy přidejte prop s názvem showMenu. Pokud bude showMenu mít hodnotu true, komponenta Header zobrazí celé menu, jako doposud. Pokud bude false, zobrazí hlavičku pouze s odkazem na hlavní stránku, jak je navrženo v zadání projektu v souboru order.jsx.
