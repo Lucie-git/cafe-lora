26a8455f815ee0fc19984bb10400302f25b55e16
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-03 11:11:55 +0200

    Add drink component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,6 @@ export const Header = () => {
     <header>
       <div className="header__content container">
         <div className="site-logo"></div>
-
         <div className="navigation">
           <button className="nav-btn"></button>
           <nav className="rollout-nav nav-closed">
