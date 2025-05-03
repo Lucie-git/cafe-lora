682525e27cd4e32bd843ad9f6f759b1db13553d5
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-03 12:56:06 +0200

    Running API Components updated to display real data."

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,18 +1,21 @@
 import { Drink } from '../drink/drink';
+import { Layer } from '../layer/layer';
 import './menu.css';
 
-export const Menu = () => {
+export const Menu = ({ drinks }) => {
   return (
-    <section className="menu">
+    <section className="menu" id="menu">
       <div className="container">
         <h2>Naše nabídka</h2>
         <p className="menu-intro">
           Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
         </p>
         <div className="drinks-list">
-          <Drink name="kafíčko1" img="/cups/doppio.png" />
-          <Drink name="kafíčko2" img="/cups/doppio.png" />
-          <Drink name="kafíčko3" img="/cups/doppio.png" />
+          {drinks.map((drink) => {
+            return (
+              <Drink key={drink.id} name={drink.name} layers={drink.layers} img={drink.image} />
+            );
+          })}
         </div>
         <div className="order-detail">
           <a href="/order.html">Detail objednávky</a>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,15 +8,25 @@ import { Gallery } from '../components/gallery/gallery';
 import { Contact } from '../components/contact/contact';
 import { Footer } from '../components/footer/footer';
 
+const response = await fetch('http://localhost:4000/api/drinks');
+const json = await response.json();
+const drinks = json.data;
+
+console.log(drinks);
+
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
     <main>
       <Banner />
-      <Menu />
+      <Menu drinks={drinks} />
       <Gallery />
       <Contact />
     </main>
     <Footer />
   </div>,
 );
+
+//mobile menu toggle
+// nav-btn
+// rolout-nav
